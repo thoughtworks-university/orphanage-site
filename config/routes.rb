d11afb093370dffe31f963be61264710b7e4d6ea
Toby Clemson <tobyclemson@gmail.com>
2010-07-13 22:16:30 +0530

    Mapping root page to home and fixing margin/padding issues in main stylesheet.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  map.root :controller => 'static_pages', :action => 'home'
   map.connect 'home', :controller => 'static_pages', :action => 'home'
   map.connect 'about-us', :controller => 'static_pages', :action => 'about_us'
   map.connect 'contact-us', :controller => 'static_pages', :action => 'contact_us'
