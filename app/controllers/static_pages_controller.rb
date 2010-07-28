47b8888f5b760b373851622d276eae27ed97c3ba
mbartlet <ThoughtWorks@MB5978s-MacBook-Pro.local>
2010-07-28 21:02:02 +0530

    Molly : fixing a couple more things with styling

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,25 +1,33 @@
 class StaticPagesController < ApplicationController
   def home
+    create_image_slideshow
 		render 'home'
 	end
 
   def about_us
+    create_image_slideshow
 	 	render 'about_us'
  	end
 
   def how_you_can_help
+    create_image_slideshow
 	 	render 'how_you_can_help'
  	end
 
   def contact_us
+    create_image_slideshow
 	 	render 'contact_us'
  	end
 
   def test
-		@images = Dir[RAILS_ROOT+'/public/images/*.jpg'] 
-		captions = Dir[RAILS_ROOT+'/public/images/*.txt']  
-		captionsArray = captions.map { |e| "\""+open(e).read+"\"" }
-		@captionsArray = "[#{captionsArray.join(",").gsub(/\n/,"")}];"
+    create_image_slideshow
 	 	render 'image_test'
  	end
+
+  def create_image_slideshow
+    @images = Dir[RAILS_ROOT+'/public/images/*.jpg'] 
+		captions = Dir[RAILS_ROOT+'/public/images/*.txt']  
+		captionsArray = captions.map { |e| "\""+open(e).read+"\"" }
+		@captionsArray = "[#{captionsArray.join(",").gsub(/\n/,"")}];" 
+  end
 end
